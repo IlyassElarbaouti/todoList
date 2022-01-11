d5a96e85af2b9766e7ae8631451c6b7b35ebd1cb
Ilyass El arbaouti <63542705+IlyassElarbaouti@users.noreply.github.com>
2022-01-11 15:31:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ const input = document.querySelector(".form__input");
 const form = document.querySelector(".form");
 const todos = document.querySelector(".todos");
 const checkIcon = document.querySelector(".drop");
+const activeBtn = document.querySelector('.active');
+const allBtn = document.querySelector('.all');
+const completedBtn = document.querySelector('.completed');
+const count = document.querySelector('.count')
 let todoList = [];
 let nextId = 1;
+
+//get active todos
+const getActive = (todoList) =>{
+  return todoList.filter(todo=>!todo.checked)
+}
+
+///render active
+const renderActive = () =>{
+renderTodos(getActive(todoList
+  ))
+}
+
+//get completed todos
+const getCompleted = (todoList) =>{
+  return todoList.filter(todo=>todo.checked)
+}
+
+///render active
+const renderCompleted = () =>{
+renderTodos(getCompleted(todoList
+  ))
+}
 
 //get clicked element index
 const getElementId = (element) => {
@@ -18,14 +44,14 @@ const deleteTodo = (event) => {
   todoList = todoList.filter(
     (todo) => todo.id !== +event.target.parentElement.id
   );
-  renderTodos();
+  renderTodos(todoList);
 };
 
 //toggle checkbox
 const toggleCheckbox = (event) => {
   const todo = getTodoById(getElementId(event.target));
   todo.checked = !todo.checked;
-  renderTodos();
+  renderTodos(todoList);
 };
 
 //add todo structure
@@ -38,8 +64,10 @@ const createTodoElement = (todo) => {
   checkbox.setAttribute("type", "checkbox");
   checkbox.addEventListener("click", toggleCheckbox);
   todo.checked ? checkbox.setAttribute("checked", true) : null;
+  checkbox.classList.add('checkbox')
 
   const label = document.createElement("h2");
+  label.classList.add('label')
   label.textContent = todo.label;
 
   const closeBtn = document.createElement("button");
@@ -54,12 +82,14 @@ const createTodoElement = (todo) => {
 };
 
 //draw todos in DOM
-const renderTodos = () => {
+const renderTodos = (todoList) => {
   todos.innerHTML = "";
   todoList.forEach((todo) => {
     todos.appendChild(createTodoElement(todo));
   });
+  setCounter(getActive(todoList))
 };
+
 //create todo
 const createTodo = (label) => {
   if (label.trim() === "") {
@@ -78,17 +108,22 @@ const submitTodo = (event) => {
   event.preventDefault();
   createTodo(input.value);
   input.value = "";
-  renderTodos();
+  renderTodos(todoList);
 };
+
+//set counter
+const setCounter = (todoList)=>{
+  count.textContent = `${todoList.length} items left`
+}
 
 //toggle checkboxs all
 const toggleCheckboxesAll = () => {
   if (!isChecked(todoList)) {
     toggleAllCheckboxes(todoList,true)
-    renderTodos();
+    renderTodos(todoList);
   } else if (isChecked(todoList)) {
     toggleAllCheckboxes(todoList,false)
-    renderTodos();
+    renderTodos(todoList);
   }
 };
 //check/uncheck all checkboxes
@@ -98,7 +133,12 @@ const toggleAllCheckboxes = (todoList,value)=>{
   });
 }
 //check if all are checked
-const isChecked = (todoList)=>todoList.every(todo=>todo.checked)
+const isChecked = (todoList)=>todoList.every(todo=>todo.checked);
 
+//event listeners
 checkIcon.addEventListener("click", toggleCheckboxesAll);
 addbtn.addEventListener("click", submitTodo);
+activeBtn.addEventListener('click',renderActive)
+completedBtn.addEventListener('click',renderCompleted)
+allBtn.addEventListener('click',()=>renderTodos(todoList))
+
